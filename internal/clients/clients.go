b2593856739a821ecbe757592ca77cc10e245e15
Jeffy Mathew <jeffy.mathew100@gmail.com>
2021-05-08 16:37:58 +0530

    add client, hasher

--- internal/clients/clients.go
+++ internal/clients/clients.go
@@ -1,1 +1,40 @@
 package clients
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type ClientInterface interface {
+	DoReq(target string) ([]byte, error)
+}
+
+type Client struct {
+	client *http.Client
+}
+
+func NewClient(cli *http.Client) ClientInterface {
+	return &Client{client: cli}
+}
+
+func (c *Client) DoReq(target string) ([]byte, error) {
+	// check if url has scheme
+	if !strings.HasPrefix("http://", target) {
+		target = "http://" + target
+	}
+	req, err := http.NewRequest(http.MethodGet, target, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
--- internal/services/hasher.go
+++ internal/services/hasher.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"encoding/hex"
+	"hash"
+)
+
+type HasherInterface interface {
+	HashInput(input []byte) string
+}
+
+type Hasher struct {
+	hash hash.Hash
+}
+
+func NewHasher(hash hash.Hash) HasherInterface {
+	return &Hasher{hash: hash}
+}
+
+func (h *Hasher) HashInput(input []byte) string {
+	return hex.EncodeToString(h.hash.Sum(input))
+}
